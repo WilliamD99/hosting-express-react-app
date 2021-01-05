177fd837ec129223b4bcd8f223ccefab47e09262
willD <dnam310199@gmail.com>
2021-01-05 00:08:17 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+
+// Enable middleware CORS
+const cors = require("cors");
+// path modules provides utilities for working with file and directory path
+const path = require("path");
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
