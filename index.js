be2df22bd2923ce7b88038d87024d50f6c80627a
willD <dnam310199@gmail.com>
2021-01-06 17:36:48 -0800

    added proxy

--- index.js
+++ index.js
@@ -12,16 +12,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-if (process.env.NODE_ENV === "production") {
-  app.get("*", (req, res) => {
-    app.use(express.static(path.join(__dirname, "client/build")));
-    res.sendFile(path.join(__dirname, "client/build", "index.html"));
-  });
-} else {
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "public", "index.html"));
-  });
-}
+app.get("*", (req, res) => {
+  app.use(express.static(path.join(__dirname, "/client/build")));
+  res.sendFile(path.join(__dirname, "/client/build", "index.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
