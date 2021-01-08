d1ea04fdde13168ed19f6f131fd807c1b28f7307
willD <dnam310199@gmail.com>
2021-01-08 00:50:38 -0800

    production

--- index.js
+++ index.js
@@ -12,8 +12,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/test", (req, res) => {
+  console.log("test");
+  return null;
+});
+
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client/build")));
   app.get("*", function (req, res) {
     res.sendFile(path.join(__dirname, "client/build", "index.html"));
   });
